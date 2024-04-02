58654b3791e4efab03fc03c633cbe8f0ba66401d
casagrande7 <joaofootz10@gmail.com>
2024-04-02 14:05:16 -0300

    Atualizado

--- app/Http/Controllers/ProdutoConctroller.php
+++ app/Http/Controllers/ProdutoConctroller.php
@@ -20,4 +20,20 @@ class ProdutoConctroller extends Controller
         });
         return response()-> json($produtosComImagem);
     }
+
+    public function store(Request $request){
+        $produtoData = $request->all();
+
+        if($request->hasFile('imagem')){
+            $imagem = $request->file('imagem');
+            $nomeImagem = time().'.'.$imagem->getClientOriginalExtension();
+            $caminhoImagem = $imagem->storeAs('imagens/produtos', $nomeImagem, 'public');
+            $produtoData['imagem'] = $caminhoImagem;
+        }
+        $produto = Produto::create($produtoData);
+        return response()->json([
+            'produto' => $produto
+        ], 201);
+         
+    }
 }
